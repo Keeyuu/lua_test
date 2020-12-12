b7d6a69b527054f04c06a520eabb87090629b242
keeYuc <52500690@qq.com>
2020-12-12 10:05:37 +0800

    dfsdfsf

--- day1202/test.cpp
+++ day1202/test.cpp
@@ -1,14 +1,77 @@
-#include<iostream>
+
+// #include <chrono>
+// #include <iostream>
+// #include <sstream>
+// #include <string>
+// #include <thread>
+
+// int test(int x, int y)
+// {
+
+//     return x + y
+// }
+// int main()
+// {
 
 
+
+
+
+//     auto sum = [](auto x, auto y)
+//     {
+//         return x + y;
+//     };
+
+
+//     std::cout << sum(10, 20
+//     ) << std::endl;
+
+    
+
+
+// }
+
+
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <regex>
 
 int main()
 {
+    std::string s = "Some people, when confronted with a problem, think "
+        "\"I know, I'll use regular expressions.\" "
+        "Now they have two problems.";
 
+    std::regex self_regex("REGULAR EXPRESSIONS",
+        std::regex_constants::ECMAScript | std::regex_constants::icase);
+    if (std::regex_search(s, self_regex))
+    {
+        std::cout << "Text contains the phrase 'regular expressions'\n";
+    }
 
+    std::regex word_regex("(\\w+)");
+    auto words_begin =
+        std::sregex_iterator(s.begin(), s.end(), word_regex);
+    auto words_end = std::sregex_iterator();
 
+    std::cout << "Found "
+        << std::distance(words_begin, words_end)
+        << " words\n";
 
+    const int N = 6;
+    std::cout << "Words longer than " << N << " characters:\n";
+    for (std::sregex_iterator i = words_begin; i != words_end; ++i)
+    {
+        std::smatch match = *i;
+        std::string match_str = match.str();
+        if (match_str.size() > N)
+        {
+            std::cout << "  " << match_str << '\n';
+        }
+    }
 
-
-
+    std::regex long_word_regex("(\\w{7,})");
+    std::string new_s = std::regex_replace(s, long_word_regex, "[$&]");
+    std::cout << new_s << '\n';
 }
